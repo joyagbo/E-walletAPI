3d500843920ff329512b881ff47d2fe4fd730eb7
joy <joyagbo49@gmail.com>
2023-07-04 16:36:32 +0100

    connected to treblle dashboard

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const { useTreblle } = require("treblle");
+require("dotenv").config();
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
 const app = express();
 const port = 4000;
+const treblle = require('@treblle/express')
 
-dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
+
+app.use(treblle());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //Setting up Treblle
 useTreblle(app, {
