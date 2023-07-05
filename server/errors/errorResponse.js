9be4042601212d7acc1ec709021bcd38c88427f6
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2023-07-04 17:03:59 -0700

    Added error handler function

---
+++ server/errors/errorResponse.js
@@ -0,0 +1,9 @@
+const errorResponse = (res, error, status = false, code = 400) => {
+    let sendData = {
+        status,
+        error
+    }
+    return res.status(code).json(sendData)
+}
+
+module.exports = {errorResponse}
