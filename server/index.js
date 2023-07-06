2d16738da22dec92a5b112512bdce2bdadeba46c
joy <joyagbo49@gmail.com>
2023-07-06 02:08:07 +0100

    updated treblle connection

--- server/index.js
+++ server/index.js
@@ -3,26 +3,17 @@ const { useTreblle } = require("treblle");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const app = express();
-// const port = 4000;
-const treblle = require('@treblle/express');
-const { custRoute } = require("./routes/customerRoute");
 const { dbConnection } = require("./config/db_connection");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
-app.use('/', custRoute)
 
-app.use(treblle());
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
 
 //Setting up Treblle
 useTreblle(app, {
   apiKey: process.env.TREBLLE_API_KEY,
-  projectId: process.env.TREBLLE_API_KEY,
+  projectId: process.env.PRODUCT_API_KEY,
 });
 
 const startServer = async () => {
@@ -40,6 +31,3 @@ const startServer = async () => {
 };
 
 startServer();
-// app.listen(process.env.PORT, () => {
-//   console.log(`Example app listening on port ${process.env.PORT}`);
-// });
